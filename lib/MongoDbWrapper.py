7eda171f5842271c5532f689a4529c2215d42f79
Roo27 <141607440+Roo27@users.noreply.github.com>
2023-09-11 14:35:03 +1000

    SDS-32-create-mongo-db-wrapper-that-handles-auth-and-i-o (#2)
    * Testing git
    
    * basic wrapper
    
    * added delete_one
    
    * single field querry and in and out
    
    * Testing git
    
    * basic wrapper
    
    * added delete_one
    
    * single field querry and in and out
    
    * fix class to be self contained
    
    * Pass args for DB Name into class
    
    * Add model
    
    * Create instance of DB in main
    
    * Clean up all users call
    
    * a
    
    * add model again
    
    * delete trash
    
    ---------
    
    Co-authored-by: Mike <michael.naughton@gmail.com>
    Co-authored-by: Michael Naughton <89134033+michael-nau@users.noreply.github.com>

---
+++ lib/MongoDbWrapper.py
@@ -0,0 +1,68 @@
+from pymongo import MongoClient
+
+class MongoDbWrapper:
+    def __init__(self, db_name="Online-Exam-System", collection_name="Users"):
+        self.client = MongoClient(
+            "mongodb+srv://Reuben:Fire@systemcluster.hwra6cw.mongodb.net/"
+        )
+        self.db = self.client[db_name]
+        self.user_collection = self.db[collection_name]
+        print("Connected to MongoDB" + db_name + " " + collection_name)
+
+    def insert_user(
+        self, id, name, loggedIn, userType, email, password, image, encodeIp
+    ):
+        new_document = {
+            "id": id,
+            "name": name,
+            "loggedIn": loggedIn,
+            "userType": userType,
+            "email": email,
+            "password": password,
+            "imageURL": image,
+            "encodeIP": encodeIp,
+        }
+        insertion_result = self.userCollection.insert_one(new_document)
+        if insertion_result.inserted_id:
+            return insertion_result.inserted_id
+        else:
+            return None
+
+    def delete_one(self, key, value):
+        query = {key: value}
+        self.userCollection.delete_one(query)
+
+    # Reutrns the whple doucment can use any key vale pair so can return muliple users
+    def query_whole_document(self, key, value):
+        query = {key: value}
+        found_documents = list(self.userCollection.find(query))
+
+        if found_documents:
+            return found_documents
+        else:
+            return None
+
+    def query_single_field(self, id_value, field_to_return):
+        query = {"id": id_value}
+        projection = self.userCollection.find_one(
+            query, {field_to_return: True, "_id": False}
+        )
+        return projection
+
+    def logged_in(self, id_value):
+        query = {"id": id_value}
+        self.userCollection.update_one(query, {"$set": {"loggedIn": True}})
+
+    def logged_out(self, id_value):
+        query = {"id": id_value}
+        self.userCollection.update_one(query, {"$set": {"loggedIn": False}})
+
+    def all_users(self):
+        all_users = self.user_collection.find({})
+        all_users = list(all_users)
+        for user in all_users:
+            user["_id"] = str(user["_id"])
+        return all_users
+
+    def close_connection(self):
+        self.client.close()
